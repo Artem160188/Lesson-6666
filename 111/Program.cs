bd8c13af6f509f1e93136dcabb6ace085c95c6f0
Artem <118046594+Artem160188@users.noreply.github.com>
2022-12-14 23:31:57 +0300

    Add files via upload

---
+++ 111/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Write("Введите количество элементов массива");
+int elementsCount = int.Parse(Console.ReadLine());
+int[] myArray = new int[elementsCount];
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"\nВведите элемент массива под индексом{i}:\t");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+int result = 0;
+foreach (int number in myArray)
+{
+    if (number > 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine($"Число элементов больше нуля: {result}");
---
+++ 333/Program.cs
@@ -0,0 +1,41 @@
+﻿// задача 40 - HARD необязательная.На вход программы подаются три 
+// целых положительных числа. Определить , является ли это сторонами 
+// треугольника. Если да, то вывести всю информацию по нему - площадь,
+// периметр, значения углов треугольника в градусах, является ли он 
+// прямоугольным, равнобедренным, равносторонним.
+Console.WriteLine("Введите 1ую сторону треугольника ");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2ую сторону треугольника ");
+double b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3ую сторону треугольника ");
+double c = Convert.ToInt32(Console.ReadLine());
+if (a + b > c && a + c > b && b + c > a)
+    Console.WriteLine("Треугольник с такими сторонами возможен");
+else
+{
+    Console.WriteLine("Это точно не треугольник");
+}
+double P = a + b + c;
+Console.WriteLine($"Периметр треугольника равен {P}");
+double S = Math.Sqrt((P / 2) * (P / 2 - a) * (P / 2 - b) * (P / 2 - c));
+Console.WriteLine($"Площадь треугольника равна {S}");
+double Alfa = Math.Round(Math.Acos(((a * a + b * b - c * c) / (2 * a * b))) * 57.295779513, 2);
+Console.WriteLine($"Угол Альфа равен {Alfa} градусов");
+double Betta = Math.Round(Math.Acos((b * b + c * c - a * a) / (2 * c * b)) * 57.295779513, 2);
+Console.WriteLine($"Угол Бетта равен {Betta} градусов");
+double Gama = Math.Round(Math.Acos((a * a + c * c - b * b) / (2 * a * c)) * 57.295779513, 2);
+Console.WriteLine($"Угол Гама равен {Gama} градусов");
+if (Alfa == Betta && Betta == Gama)
+{
+    Console.WriteLine("Треугольник равносторонний");
+}
+else if (Alfa == Betta || Betta == Gama || Gama == Alfa)
+{
+    Console.WriteLine("Треугольник равнобедренный");
+}
+
+else if (Alfa == 90 || Betta == 90 || Gama == 90)
+{
+    Console.WriteLine("Треугольник прямоугольный");
+}
+
